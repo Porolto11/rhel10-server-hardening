e7111287f4fe79464992c939a276a4b168332bf5
Hódi Richárd <129577413+Porolto11@users.noreply.github.com>
2026-01-22 09:09:27 +0100

    Update auto-patch.sh

--- auto-patch.sh
+++ auto-patch.sh
@@ -9,3 +9,6 @@ echo "Rebooting if needed..."
 if [ -f /var/run/reboot-required ]; then sudo reboot; fi
 
 #Auto-Patch Script
+#I also added a crontab automation
+#sudo crontab -e and added the line with the help of documentations: 0 3 * * 0 /full/path/auto-patch.sh >> /var/log/auto-patch.log 2>&1 then verified it with sudo crontab -l command
+#this automation start on every sunday 3:00 AM and runs the script automatically and saves output for possible errors to the log file.
